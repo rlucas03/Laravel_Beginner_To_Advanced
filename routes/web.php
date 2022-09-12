3bc2eea70ddd625bb740ce53522cb6fb838a5d6d
Ryan Lucas <ryanlucas@moo.home>
2022-09-12 20:14:54 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Laravel 8 (new)
+Route::get('/', [PagesController::class, 'index']);
+Route::get('about', [PagesController::class, 'about']);
+
+
+
+
+//before Laravel 8 outdated 
+//Route::get('/products', 'ProductsController@index');
+
+// firstproject.com == /
+// firstproject.com/users == /users or users
