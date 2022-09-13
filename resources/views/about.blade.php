d01830e2a6c99118bbc4de8e9bb7afc98a4cb974
Ryan Lucas <ryanlucas@moo.local>
2022-09-13 11:36:26 +0100

    experimenting in about page with loops and control

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,33 +1,31 @@
-@[structure]
-{{-- control structures --}}
-
-
-@[endstucture]
-@if (5 > 10)
-<p>5 is lower than 10</p>
-@elseif(5==10)
-<p>5 is lower than 10</p>
-@else
-    <h2>all conditions are wrong</h2>
-@endif
-
-@unless (empty($name))
-    <h2>name isn't empty</h2>
+{{-- 
+    Comparing multiple possible conditions
+    Multiple values thast may require the same code
     
-@endunless
-
-@if (!empty($name))
-<h2>name isn't empty</h2>
-
-@endif
-
-@empty($name)
-<h2>name is empty</h2>
-
-@endempty
-@isset($name)
-<h2>name is set</h2>
-
     
-@endisset
+    
+    
+    
+    --}}
 
+    @switch($name)
+        @case('dar')
+            <h2>Name is equal to Ryan</h2>
+            @break
+        @case('Name is John')
+            <h2>Hello John</h2>
+            @break    
+        @case('Michael')
+             <h2>Hello Michael</h2>
+                
+            @break
+        @default
+        <h2>no match found<h2>
+            
+    @endswitch
+
+    <!--
+    
+    test
+
+-->
